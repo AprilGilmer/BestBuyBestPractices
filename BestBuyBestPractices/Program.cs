ab2d964c6bba80a3238f1e14747ca4d4063bb4b4
April Gilmer <aprilgilmer1@gmail.com>
2025-07-24 17:53:13 -0500

    Completed BestBuyBestPractices Exerice 2

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -11,18 +11,34 @@ var config = new ConfigurationBuilder()
 string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
+#region Department Section
+//var departmentRepo = new DapperDepartmentRepository(conn);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+//departmentRepo.InsertDepartment("April's New Department");
 
-departmentRepo.InsertDepartment("April's New Department");
+//var departments = departmentRepo.GetAllDepartments();
 
-var departments = departmentRepo.GetAllDepartments();
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//    Console.WriteLine();
 
-foreach (var department in departments)
-{
-    Console.WriteLine(department.DepartmentID);
-    Console.WriteLine(department.Name);
+//}
+#endregion
+
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+
+foreach (var product in products)
+{ 
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name); 
+    Console.WriteLine(product.Price); 
+    Console.WriteLine(product.CategoryID); 
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
     Console.WriteLine();
     Console.WriteLine();
-
 }
